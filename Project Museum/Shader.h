26eccd8172dde540bad48993a4aabf6c713919a8
Ronald Allan Baldonado <ronbaldona@gmail.com>
2019-04-11 20:10:50 -0700

    Adding unsaved changes

--- Project Museum/Shader.h
+++ Project Museum/Shader.h
@@ -1,1 +1,24 @@
 #pragma once
+#include "variables.h"
+
+/************************************* DELETE LATER ****************************************/
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+/************************************* DELETE LATER ****************************************/
+
+void createShaders() {
+	vertShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertShader, 1, &vertexShaderSource, NULL);
+
+}
--- Project Museum/variables.h
+++ Project Museum/variables.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+typedef glm::mat3 mat3;
+typedef glm::mat4 mat4;
+typedef glm::vec3 vec3;
+typedef glm::vec4 vec4;
 
 #ifdef MAINPROGRAM 
 #define EXTERN 
@@ -9,7 +17,6 @@
 #endif
 
 EXTERN GLFWwindow* window = nullptr;
-
 EXTERN unsigned int vertShader;
 EXTERN unsigned int fragShader;
 
